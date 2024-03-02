fe4c6b2cd7cc0ef24714287ccf0e873d9f550ba4
Oshankiii12 <122991579+Oshankiii12@users.noreply.github.com>
2024-03-02 15:49:13 +0530

    added routes and response codes

---
+++ utils/password.js
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv';
+import crypto from 'crypto';
+import jwt from 'jsonwebtoken';
+
+dotenv.config();
+
+const SECRET = process.env.SECRET || 'default_secret'; // Use the environment variable or a default value
+
+export function getJwt(object, expiresIn = '30d') {
+  return jwt.sign(object, SECRET, { expiresIn });
+}
+
+export async function hash_password(password) {
+  return new Promise((resolve, reject) => {
+    const salt = crypto.randomBytes(16).toString('hex');
+
+    crypto.scrypt(password, salt, 64, (err, derivedKey) => {
+      if (err) reject(err);
+      resolve(`${salt}:${derivedKey.toString('hex')}`);
+    });
+  });
+}
